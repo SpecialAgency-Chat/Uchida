ff31c0951a08e656720bfcdf5c2b20fbafb02f45
waki285 <suzuneu@suzuneu.com>
2023-08-05 12:40:30 +0900

    invite link

--- src/app/invites/page.tsx
+++ src/app/invites/page.tsx
@@ -5,6 +5,25 @@ export default async function Page({
   params: { slug: string };
   searchParams?: { [key: string]: string | string[] | undefined };
 }) {
-  const d = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/invites?code=${searchParams?.code}`);
-  return <h1>{searchParams?.greeting || await d.text()}</h1>;
+  if (!process.env.NEXT_PUBLIC_INVITE_REDIRECT_URI) {
+    throw new Error("Developer Error: NEXT_PUBLIC_INVITE_REDIRECT_URI is not set");
+  }
+  const d = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/invites?code=${searchParams?.code}`,
+  );
+  const json = await d.json();
+  if (json.error) {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24 text-lg">
+        <p>Error: ${json.error}</p>
+      </main>
+    );
+  } else {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24 text-lg">
+        <p className="mb-6">Congrats.</p>
+        <p className="px-3 py-2 rounded-full bg-emerald-500 hover:bg-emerald-600"><a href={`https://discord.com/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.NEXT_PUBLIC_INVITE_REDIRECT_URI)}&response_type=code&prompt=none&scope=identify%20guilds.join%20role_connections.write&state=${json.code}`}>Join Server</a></p>
+      </main>
+    )
+  }
 }
