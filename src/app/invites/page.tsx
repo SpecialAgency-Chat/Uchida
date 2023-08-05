5db7238e42f01300739ac325b0eb52b738b82fbf
waki285 <suzuneu@suzuneu.com>
2023-08-05 12:44:14 +0900

    link

--- src/app/invites/page.tsx
+++ src/app/invites/page.tsx
@@ -15,14 +15,14 @@ export default async function Page({
   if (json.error) {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24 text-lg">
-        <p>Error: ${json.error}</p>
+        <p>Error: {json.error}</p>
       </main>
     );
   } else {
     return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 text-lg">
+      <main className="flex min-h-screen flex-col items-center p-24 text-lg">
         <p className="mb-6">Congrats.</p>
-        <p className="px-3 py-2 rounded-full bg-emerald-500 hover:bg-emerald-600"><a href={`https://discord.com/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.NEXT_PUBLIC_INVITE_REDIRECT_URI)}&response_type=code&prompt=none&scope=identify%20guilds.join%20role_connections.write&state=${json.code}`}>Join Server</a></p>
+        <a href={`https://discord.com/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.NEXT_PUBLIC_INVITE_REDIRECT_URI)}&response_type=code&prompt=none&scope=identify%20guilds.join%20role_connections.write&state=${json.code}`} className="px-3 py-2 rounded-full bg-emerald-500 hover:bg-emerald-600" role="button">Join Server</a>
       </main>
     )
   }
