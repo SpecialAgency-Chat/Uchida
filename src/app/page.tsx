8f142359dc2b64ceb60008b426ea9c73fd3fcff1
waki285 <suzuneu@suzuneu.com>
2023-08-06 12:13:12 +0900

    fix button style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <p className="mb-[13rem]">Hello</p>
       <p className="mb-[23rem] text-transparent mr-[60%] hover:underline hover:text-slate-900"><Link href="/login">Or... You are the member of SAC?</Link></p>
       <p className="my-[36rem]">Why are you still here?</p>
-      <p className="px-3 py-2 rounded-full bg-emerald-500 hover:bg-emerald-600" role="button"><a href={`https://discord.com/api/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.NEXT_PUBLIC_REQUEST_REDIRECT_URI)}&response_type=code&scope=identify%20guilds.join%20role_connections.write&prompt=none`}>Join Request</a></p>
+      <a href={`https://discord.com/api/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.NEXT_PUBLIC_REQUEST_REDIRECT_URI)}&response_type=code&scope=identify%20guilds.join%20role_connections.write&prompt=none`} className="px-3 py-2 rounded-full bg-emerald-500 hover:bg-emerald-600" role="button">Join Request</a>
     </main>
   );
 }
